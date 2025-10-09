d2d1b32bc05cb1586a2997922f55c8ac10f2cb8e
tunnu-sahni <tsahni313@gmail.com>
2025-10-09 22:29:35 +0530

    added twodimarr.py

---
+++ twodimarr.py
@@ -0,0 +1,57 @@
+import numpy as np
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr)
+
+
+import numpy as np
+arr = np.array([[2,4,5,6,7],[6,6,7,5,6]])
+print(arr)
+print(type(arr))
+print(len(arr))
+print("dimenson:",arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([[12,12,3,3],[3,44,55,66]])
+print(arr)
+print(type(arr))
+print("dimension:",arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([[12,1,22,3,44,45,56,5,4,3],[43,5,11,2,22,3,77,8,8,99]])
+print(arr)
+print("dimension:",arr.ndim)
+
+
+
+
+
+
+#three dimension array
+
+
+import numpy as np
+arr = np.array([[[1,2,3,4],[5,6,7,8],[9,0,2,3]]])
+print(arr)
+print(type(arr))
+print("dimension:",arr.ndim)
+
+
+
+
+import numpy as np
+arr = np.array([[[1,33,44],[44,55,4],[66,7,88]]])
+print(arr)
+print("dimension:",arr.ndim)
+print(len(arr))
+
+
+
+
+import numpy as np
+arr = np.array([[[2,3,44,55],[1,22,3,4],[55,6,6,7]]])
+print(arr)
+print("dimension:",arr.ndim)
