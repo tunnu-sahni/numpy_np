d2c604d76f0bd010f3a9a88de04336b247397fde
tunnu-sahni <tsahni313@gmail.com>
2025-10-09 21:38:31 +0530

    added onedimarr.py

---
+++ onedimarr.py
@@ -0,0 +1,37 @@
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(arr)
+print("dimension:",arr.ndim)
+
+
+import numpy as np
+arr = np.array([5,6,7,8,9,0])
+print(arr)
+print("dimension:",arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([112,33,44,5,6,66,4,23])
+print(arr)
+print(len(arr))
+print(type(arr))
+print("dimension:",arr.ndim)
+
+
+
+import numpy as np
+arr = np.array(arr)
+print(arr)
+print("dimension:",arr.ndim)
+
+
+
+
+
+import numpy as np
+arr = np.array([43,54,56,67,78,89,90])
+print(arr)
+print(len(arr))
+print(type(arr))
+print("dimension:",arr.ndim)
