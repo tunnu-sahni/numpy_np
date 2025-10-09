91e23f56dd403e4873c38edebbe34cff51029197
tunnu-sahni <tsahni313@gmail.com>
2025-10-09 18:18:25 +0530

    zerodimarray.py

---
+++ zerodimarray.py
@@ -0,0 +1,31 @@
+import numpy as np
+arr = np.array(42)
+print(arr)
+print("dimension:", arr.ndim)
+
+
+
+import numpy as np 
+arr = np.array(7)
+print(arr)
+print("dimension:",arr.ndim)
+
+
+import numpy as np
+arr = np.array(758)
+print(arr)
+print("dimension:",arr.ndim)
+
+
+import numpy as np
+arr = np.array(734759)
+print(arr)
+print(type(arr))
+print("dimension:",arr.ndim)
+
+
+
+import numpy as np
+arr = np.array(4664)
+print(arr)
+print("dimension:",arr.ndim)
