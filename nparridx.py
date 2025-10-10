830e5cb97f295e70e71e72f2e7efbbcbea61bfba
tunnu-sahni <tsahni313@gmail.com>
2025-10-10 21:59:26 +0530

    nparridx.py

---
+++ nparridx.py
@@ -0,0 +1,44 @@
+import numpy as np
+arr = np.array([1,2,3,4])
+print(arr[0])
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7])
+print([1])
+
+
+
+import numpy as np
+arr = np.array([11,22,3,33,4,44])
+print([3])
+print("dimension:",arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([22,3,33,6,77,88,8])
+print(arr[4]+ arr[5])
+
+
+
+import numpy as np
+arr = np.array([2,3,3,44,5,5,6,7,8])
+print(arr[3] + arr[5])
+print("dimension:",arr.ndim)
+
+
+
+
+
+import numpy as np
+arr = np.array([44,55,666,77,8,88,24,453])
+print(arr[4] + arr[6])
+print("dimension:", arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([2,33,4,55,6,66,77,77,2])
+print(arr[3] + arr[5])
