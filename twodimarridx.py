5ae8d8c9ae33e78db907ab3289bdfadba5814165
tunnu-sahni <tsahni313@gmail.com>
2025-10-10 22:52:23 +0530

    twodimarridx.py

---
+++ twodimarridx.py
@@ -0,0 +1,46 @@
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+print('2 element on 1 row:',arr[0,1])
+
+
+
+
+
+import numpy as np
+arr = np.array([[3,4,5,6],[7,8,9,0]])
+print('4 element on 2 row:', arr[1,3])
+
+
+
+import numpy as np
+arr = np.array([[1,2,3],[5,6,7]])
+print('3 element on 0 row:',arr[1,2])
+
+
+
+
+import numpy as np
+arr = np.array([[23,55,6,66,4],[2,3,44,55,6]])
+print('4 element on 5 row:',arr[0,3])
+
+
+
+
+
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print('2 element on 1 row :',arr[1,2])
+print(arr[0,0])
+print(arr[1,2])
+print(arr[2,:])
+print(arr[0,2])
+print(arr[:,1])
+print(arr[:,2])
+print(arr[:,0])
+
+
+
+import numpy as np
+arr = np.array([[1,3,4],[4,5,6],[33,4,66]])
+print(arr[1,2])
+print(arr[:,2])
