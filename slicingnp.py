b936dc73334e59945d552966a8138ef889795d39
tunnu-sahni <tsahni313@gmail.com>
2025-10-10 22:15:12 +0530

    slicingnp.py

---
+++ slicingnp.py
@@ -0,0 +1,44 @@
+import numpy as np
+arr = np.array([10,20,30,40,50,60])
+print(arr[1:4])
+
+
+
+import numpy as np
+arr = np.array([2,33,44,5,66,77,8,99,8,77])
+print(arr[2:4])
+
+
+
+import numpy as np
+arr = np.array([33,4,5,66,7])
+print(arr[:])
+print(arr[::])
+
+
+
+
+import numpy as np
+arr = np.array([3,4,55,66,7,77,8,88])
+print(arr[:3])
+
+
+
+
+import numpy as np
+arr = np.array([3,22,333,44,55,55])
+print(arr[::4])
+
+
+
+import numpy as np
+arr = np.array([1,22,33,44,55,5,5,33])
+print(arr[5::])
+
+
+
+
+import numpy as np
+arr = np.array([33,44,55,5,66,77,7,777])
+print(arr[2:6])
+print("dimension:",arr.ndim)
