5eefecc3fd08d3157be28a9e3f8d7f034b02a655
tunnu-sahni <tsahni313@gmail.com>
2025-10-12 18:19:52 +0530

    threearridx.py

---
+++ threearridx.py
@@ -0,0 +1,33 @@
+import numpy as np
+arr = np.array([[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]])
+print(arr[0,1,2])
+
+
+
+
+import numpy as np
+arr = np.array([[[10,20,30],[40,50,60],[70,80,90],[100,120,130]]])
+print(arr[0,2,2])
+
+
+
+
+import numpy as np
+arr = np.array([[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]])
+print(arr[0,2,3])
+
+
+
+
+import numpy as np
+arr = np.array([[[12,13,14,15],[16,17,18,19],[20,21,22,23],[24,25,26,27]]])
+print(arr[0,2,3])
+
+
+
+
+
+import numpy as np
+arr = np.array([[[21,32,43,54,46],[54,56,76,45,43],[23,345,54,76,58,],[56,876,98,765,6]]])
+print(arr[0,3,4])
+print(arr[0,2,4])
