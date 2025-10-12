b3890dd7fc257bba11bf59db5747fd89114bd457
tunnu-sahni <tsahni313@gmail.com>
2025-10-12 17:44:28 +0530

    fancyidx.py

---
+++ fancyidx.py
@@ -0,0 +1,56 @@
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print(arr[[0,2,4]])
+
+
+import numpy as np
+arr = np.array([10,20,30,40,50,60,70,80])
+print(arr[[1,3,7]])
+
+
+
+
+import numpy as np
+arr = np.array([12,1,13,14,24,34,34,35,45,5555555,56])
+print(arr[[1,2,8]])
+print(arr[arr%2 ==0])
+
+
+
+import numpy as np
+arr = np.array([12,13,14,15,24,25,26,446,47])
+print(arr[[1,2,7]])
+
+
+
+
+import numpy as np
+arr = np.array([[10,20,30],[40,50,60],[70,80,90]])
+print(arr[[0,2],[1,2]])
+print(arr[[0,1],[2,2]])
+print(arr[[0,2],[1,2]])
+
+
+
+import numpy as np
+arr = np.array([[10,20,30,40],[23,34,4,56],[45,56,67,87]])
+print(arr[[0,1],[1,2]])
+
+
+
+
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr[[0,1],[1,2]])
+
+
+
+
+import numpy as np
+arr = np.array([[2,3,4],[5,6,7],[8,9,0]])
+print(arr[[0,1],[1,2]])
+
+
+import numpy as np
+arr = np.array([[11,22,33],[44,55,66],[77,88,99]])
+print(arr[[1,2],[2,2]])
