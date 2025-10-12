13a4df40b787f3168c47a589c189242d4fe413c7
tunnu-sahni <tsahni313@gmail.com>
2025-10-12 22:08:07 +0530

    nparrslicing.py

---
+++ nparrslicing.py
@@ -0,0 +1,24 @@
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7])
+print(arr[1:5:2])
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7])
+print(arr[1:7:2])
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6])
+print(arr[0:5:1])
+
+
+
+import numpy as np
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr[0:1:2])
+
+
+
