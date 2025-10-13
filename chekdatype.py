b1591e11a0f1df8b84b2b06a4134b966712c90f5
tunnu-sahni <tsahni313@gmail.com>
2025-10-13 10:36:08 +0530

    chekdatype.py

---
+++ chekdatype.py
@@ -0,0 +1,28 @@
+import numpy as np
+arr = np.array([1,2,3,4])
+print(arr.dtype)
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(arr.dtype)
+
+
+import numpy as np
+arr = np.array([2,3,4,5])
+print(arr.dtype)
+
+
+#datatype string
+
+
+import numpy as np
+arr = np.array(['apple', 'banana', 'chery'])
+print(arr.dtype)
+
+
+
+import numpy as np
+arr = np.array(['mango', 'orange', 'apple', 'grapes'])
+print(arr.dtype)
