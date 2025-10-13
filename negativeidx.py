b10ecef82e57b771550e1e67ce37da48b390f04d
tunnu-sahni <tsahni313@gmail.com>
2025-10-13 10:04:08 +0530

    negativeidx.ppy

---
+++ negativeidx.py
@@ -0,0 +1,17 @@
+import numpy as np
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+print('last element from 2 dim:', arr[1,-1])
+
+
+
+
+import numpy as np
+arr = np.array([[12,13,14,15,16],[17,18,19,20,21]])
+print('first element from 3 dim:', arr[0,-3])
+
+
+
+
+import numpy as np
+arr = np.array([[2,3,4,5],[6,7,8,9]])
+print('first element from 3 dim:', arr[0,-2])
