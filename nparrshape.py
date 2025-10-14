90e8ae61d49c7a1f53a8c93822672ead70561ac2
tunnu-sahni <tsahni313@gmail.com>
2025-10-14 20:50:32 +0530

    nparrshape.py

---
+++ nparrshape.py
@@ -0,0 +1,37 @@
+import numpy as np
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr.shape)
+
+
+
+import numpy as np
+arr = np.array([[10,20,30,40],[50,60,70,80]])
+print(arr.shape)
+print("dimension:" ,arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([[2,3,4,5,6],[9,7,8,9,0]])
+print(arr.shape)
+
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4], ndmin=5)
+print(arr)
+print('shape of array:', arr.shape)
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7,8,9], ndmin=10)
+print(arr)
+print('shape of array:', arr.shape)
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6],ndmin=5)
+print(arr)
+print('shape of array:', arr.shape)
