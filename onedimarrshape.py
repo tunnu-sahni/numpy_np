43e9cb01d085292003bac32d9cf05a20cd7e6780
tunnu-sahni <tsahni313@gmail.com>
2025-10-14 21:18:18 +0530

    onedimarrshape.py

---
+++ onedimarrshape.py
@@ -0,0 +1,80 @@
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print(arr.shape)
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,67])
+print(arr.shape)
+print("dimension array:", arr.ndim)
+
+
+import numpy as np
+arr = np.array([1,2,3,4])
+print(arr.shape)
+
+
+
+
+
+# two dimension array shape
+
+
+import numpy as np
+arr = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr.shape)
+print("dimension array:", arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([[2,3,44,5,2],[43,54,56,7,7]])
+print(arr.shape)
+
+
+
+import numpy as np
+arr = np.array([[22,33,44,55],[66,77,88,99]])
+print(arr.shape)
+print("dimension array:", arr.ndim)
+
+
+
+import numpy as np
+arr = np.array([[1,2,3,3,3],[44,55,6,6,6]])
+print(arr.shape)
+
+
+
+
+# three dimension array shape
+
+
+import numpy as np
+arr = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(arr.shape)
+print('shape of array:')
+
+
+
+import numpy as np
+arr = np.array([[[10,20,30,40],[50,60,70,80],[90,100,110,120]]])
+print(arr.shape)
+
+
+
+import numpy as np
+arr = np.array([[[32,43],[45,56],[23,12]]])
+print(arr.shape)
+
+
+
+import numpy as np
+arr = np.array([[[11,22,33],[44,55,66],[77,88,99]]])
+print(arr.shape)
+
+
+
+import numpy as np
+arr = np.array([[[23],[43],[5]]])
+print(arr.shape)
