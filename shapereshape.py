a2013e888c03873780958eb3b00a2ad0f639aef1
tunnu-sahni <tsahni313@gmail.com>
2025-10-14 21:30:48 +0530

    shapereshape.py

---
+++ shapereshape.py
@@ -0,0 +1,22 @@
+import numpy as np
+arr = np.array([1,2,3,4,5,6])
+new_arr = arr.reshape(2,3)
+print(new_arr)
+print(new_arr.shape)
+
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6])
+new_arr = arr.reshape(3,2)
+print(new_arr)
+print(new_arr.shape)
+
+
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7,8,9])
+new_arr = arr.reshape(3,3)
+print(new_arr)
+print(new_arr.shape)
