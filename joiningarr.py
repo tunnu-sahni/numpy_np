82f70fdfc2bb7c31eda33a804b784be709f34493
tunnu-sahni <tsahni313@gmail.com>
2025-11-06 20:45:30 +0530

    added joiningarr.py

---
+++ joiningarr.py
@@ -0,0 +1,25 @@
+import numpy as np
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+arr = np.concatenate((arr1,arr2))
+print(arr)
+
+
+
+
+import numpy as np
+arr1 = np.array([10,20,30])
+arr2 = np.array([40,50,60])
+
+arr = np.concatenate((arr1, arr2))
+print(arr)
+
+
+
+import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([5,6,7,8])
+
+arr = np.concatenate((arr1, arr2))
+print(arr)
