3520d4a00beafaa93a1a1d1d1f024c66941068b7
tunnu-sahni <tsahni313@gmail.com>
2025-11-06 21:42:30 +0530

    added randomnum.py

---
+++ randomnum.py
@@ -0,0 +1,15 @@
+from numpy import random
+x = random.randint(100)
+print(x)
+
+
+
+from numpy import random 
+x = random.randint(12)
+print(x)
+
+
+
+from numpy import random
+x = random.rand()
+print(x)
