0366a94d799d00cf6646414fd8de2eb1279e5d07
tunnu-sahni <tsahni313@gmail.com>
2025-11-06 22:36:35 +0530

    added randomarr.py

---
+++ randomarr.py
@@ -0,0 +1,92 @@
+# from numpy import random
+# x = random.randint(100, size=(5))
+# print(x)
+
+
+
+# from numpy import random
+# x = random.randint(10, size=(3))
+# print(x)
+
+
+
+# from numpy import random
+# x = random.randint(120, size=(6))
+# print(x)
+
+
+
+
+# # generate 2-D random number
+
+
+# from numpy import random
+# x = random.randint(100, size=(3,5))
+# print(x)
+
+
+
+# from numpy import random
+# x = random.randint(120, size=(2,4))
+# print(x)
+
+
+
+# from numpy import random
+# x = random.randint(1000, size=(12,3))
+# print(x)
+
+
+# # float random number
+
+
+# from numpy import random
+# x = random.rand(5)
+# print(x)
+
+
+
+# from numpy import random
+# x = random.rand(3)
+# print(x)
+
+
+
+# from numpy import random
+# x = random.rand(6)
+# print(x)
+
+
+
+# float 2-D random number
+
+
+# from numpy import random
+# x = random.rand(2,3)
+# print(x)
+
+
+
+# from numpy import random
+# x = random.rand(1,3)
+# print(x)  
+
+
+
+# # random nunber choice methods
+
+
+# from numpy import random
+# x = random.choice([5,4,2,6,7])
+# print(x)
+
+
+
+from numpy import random
+x = random.choice([3,4,2,5,7,9], size=(3,5))
+print(x)
+
+
+from numpy import random
+x = random.choice([21,32,4,3,54,6], size=(5,2))
+print(x)
