a4f7d6396c65794c23c81b8260e0f19ccff6a61e
tunnu-sahni <tsahni313@gmail.com>
2025-11-06 21:05:11 +0530

    added stackingrows.py

---
+++ stackingrows.py
@@ -0,0 +1,57 @@
+import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([5,6,7,8])
+
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+
+
+import numpy as np
+arr1 = np.array([12,13,14])
+arr2 = np.array([15,16,17])
+
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+
+
+import numpy as np
+arr1 = np.array([11,12,1])
+arr2 = np.array([34,35,3])
+
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+
+
+
+
+# stacking along column
+
+import numpy as np
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+arr = np.vstack((arr1, arr2))
+print(arr)
+
+
+
+
+import numpy as np
+arr1 = np.array([10,20,30,40])
+arr2 = np.array([50,60,70,80])
+
+arr = np.vstack((arr1, arr2))
+print(arr)
+
+
+
+
+import numpy as np
+arr1 = np.array([5,6])
+arr2 = np.array([7,8])
+
+arr = np.vstack((arr1, arr2))
+print(arr)
