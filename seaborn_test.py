14e329f53fb5b35e61abeb9a232a4117a2a9cedb
tunnu-sahni <tsahni313@gmail.com>
2025-11-07 21:10:50 +0530

    added seaborn_test.py

---
+++ seaborn_test.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot([0,1,2,3,4,5])
+
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot([10,15,16,123,11])
+
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot([0,1,2,3,4,5], kind="kde")
+
+plt.show()
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot([10,20,30,40,50])
+
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot([1,2,3], kind="kde")
+
+plt.show()
