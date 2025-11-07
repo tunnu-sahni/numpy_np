7e633671dc3cbad5c46587d84bba15595750fb21
tunnu-sahni <tsahni313@gmail.com>
2025-11-07 10:55:54 +0530

    added rmdpermutation.py

---
+++ rmdpermutation.py
@@ -0,0 +1,56 @@
+# from numpy import random
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# random.shuffle(arr)
+# print(arr)
+
+
+
+# from numpy import random
+# import numpy as np
+# arr = np.array([1,2,3])
+# random.shuffle(arr)
+# print(arr)
+
+
+
+# from numpy import random
+# import numpy as np
+# arr = np.array([1,2,3,4])
+# random.shuffle(arr)
+# print(arr)
+
+
+
+from numpy import random
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(random.permutation(arr))
+
+
+from numpy import random
+import numpy as np
+arr = np.array([1,2,3])
+print(random.permutation(arr))
+
+
+
+from numpy import random
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(random.permutation(arr))
+
+
+
+from numpy import random
+import numpy as np
+arr = np.array([1,2,3])
+random.shuffle(arr)
+print(arr)
+
+
+
+from numpy import random
+import numpy as np
+arr = np.array([1,2,3,4])
+print(random.permutation(arr))
