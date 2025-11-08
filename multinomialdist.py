5f045c1a2f602b29f5baa9f2c0b9b53ffbc500d9
tunnu-sahni <tsahni313@gmail.com>
2025-11-08 17:02:33 +0530

    added multinomialdist.py

---
+++ multinomialdist.py
@@ -0,0 +1,16 @@
+from numpy import random
+x = random.multinomial(n=6, pvals=[1/6, 1/6, 1/6, 1/6, 1/6,1/6])
+
+print(x)
+
+
+from numpy import random
+x = random.multinomial(n=5, pvals=[1/5, 1/5, 1/5,1/5, 1/5])
+
+print(x)
+
+
+from numpy import random
+x = random.multinomial(n=3, pvals=[1/3,1/3,1/3])
+
+print(x)
