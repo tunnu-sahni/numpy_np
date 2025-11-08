43ad693a8676c07e4981b06f6ece4477a983df0c
tunnu-sahni <tsahni313@gmail.com>
2025-11-08 18:48:17 +0530

    added exponentialdist.py

---
+++ exponentialdist.py
@@ -0,0 +1,20 @@
+from numpy import random
+
+x = random.exponential(scale=2, size=(2, 3))
+
+print(x)
+
+
+
+from numpy import random
+
+x = random.exponential(scale=3, size=(3, 5))
+
+print(x)
+
+
+from numpy import random
+
+x = random.exponential(scale=2, size=(2, 5))
+
+print(x)
