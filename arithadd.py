717a9435b79fb349eb5702ff088ab5872941fa7b
tunnu-sahni <tsahni313@gmail.com>
2025-11-09 22:23:02 +0530

    added arithadd.py

---
+++ arithadd.py
@@ -0,0 +1,260 @@
+import numpy as np
+
+arr1 = np.array([10,11,12,13,14,15])
+arr2 = np.array([20,21,22,23,23,24])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([22,33,44])
+arr2 = np.array([22,445,66])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([44,55,66,77,88])
+arr2 = np.array([22,44,66,77,99])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([33,22,44])
+arr2 = np.array([55,66,77])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
+
+
+
+# subtraction 
+
+
+import numpy as np
+
+arr1 = np.array([22,44,5,66])
+arr2 = np.array([44,55,66,88])
+
+newarr = np.subtract(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([22,3,44,556,77])
+arr2 = np.array([55,6,77,88,99])
+
+newarr = np.subtract(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([38,885,77,88,7])
+arr2 = np.array([22,33,5,66,77])
+
+newarr = np.subtract(arr1, arr2)
+
+print(newarr)
+
+
+# multiply
+
+
+import numpy as np
+
+arr1 = np.array([2,33,44,55])
+arr2 = np.array([4,5,66,77])
+
+newarr = np.multiply(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([3,4,5])
+arr2 = np.array([5,6,7])
+
+newarr = np.multiply(arr1, arr2)
+
+print(newarr)
+
+
+
+
+#division
+
+
+
+import numpy as np
+
+arr1 = np.array([2,3,4,5])
+arr2 = np.array([5,7,8,9])
+
+newarr = np.divide(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr2 = np.array([4,5,6,7])
+arr3 = np.array([6,7,8,9])
+
+newarr =  np.divide(arr2, arr3)
+
+print(newarr)
+
+
+
+# power
+
+
+import numpy as np
+
+arr1 = np.array([2,3,5,6])
+arr2 = np.array([4,5,6,7])
+
+newarr = np.power(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([2,3,4])
+arr2 = np.array([4,5,6])
+
+newarr = np.power(arr1, arr2)
+
+print(newarr)
+
+
+
+# remainder
+
+
+import numpy as np
+
+arr1  = np.array([2,3,4,5])
+arr2 = np.array([2,5,6,7])
+
+newarr = np.mod(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([3,4,5])
+arr2 = np.array([5,6,7])
+
+newarr = np.mod(arr1, arr2)
+
+print(newarr)
+
+
+import numpy as np
+
+arr1 = np.array([2,4,5])
+arr2 = np.array([4,5,6])
+
+newarr = np.remainder(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([3,4,5,6])
+arr2 = np.array([5,6,7,8])
+
+newarr = np.remainder(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([4,5,6])
+arr2 = np.array([5,6,7])
+
+newarr = np.remainder(arr1, arr2)
+
+print(newarr)
+
+
+
+# quotient and mod
+
+
+import numpy as np
+
+arr1 = np.array([2,3,4])
+arr2 = np.array([4,5,6])
+
+newarr = np.divmod(arr1, arr2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr1 = np.array([3,4,5,6])
+arr2 = np.array([5,6,7,8])
+
+newarr = np.divmod(arr1, arr2)
+
+print(newarr)
+
+
+
+
+# absolute values
+
+
+import numpy as np
+
+arr1 = np.array([2,3,4])
+arr2 = np.array([4,5,6])
+
+newarr = np.absolute(arr1, arr2)
+
+print(newarr)
+
+
+import numpy as np
+
+arr1 = np.array([3,4,5])
+arr2 = np.array([6,7,8])
+
+newarr = np.absolute(arr1 , arr2)
+
+print(newarr)
