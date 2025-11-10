daf85ed0e469c9003391cfb414bf5ca20b56ba44
tunnu-sahni <tsahni313@gmail.com>
2025-11-10 22:02:25 +0530

    added ceildecimal.py

---
+++ ceildecimal.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.ceil([-3.1666, 3.6667])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.ceil([-4.555, 6.777])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.ceil([44.555, 66.777])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.ceil([3.55, 6.66])
+
+print(arr)
