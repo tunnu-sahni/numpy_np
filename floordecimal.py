ca65dc14529acfbf4ab2e2d241a7f0ae7faba9bc
tunnu-sahni <tsahni313@gmail.com>
2025-11-10 21:55:21 +0530

    added floordecimal.py

---
+++ floordecimal.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr = np.floor([-3.1666, 3.6667])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.floor([-4.1555, 4.5666])
+
+print(arr)
+
+
+
+import numpy as np
+
+arr = np.floor([-55.333, 88.666])
+
+print(arr)
