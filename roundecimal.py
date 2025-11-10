3c9c1084c8b61fd0be8e2ccc651177f62837777c
tunnu-sahni <tsahni313@gmail.com>
2025-11-10 21:54:16 +0530

    added roundecimal.py

---
+++ roundecimal.py
@@ -0,0 +1,47 @@
+# truncation 
+
+# import numpy as np
+
+# arr = np.trunc([-3.1666, 3.6667])
+
+# print(arr)
+
+
+import numpy as np
+
+arr = np.trunc([2.33, 4.55])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.trunc([3.444, 6.777])
+
+print(arr)
+
+
+
+# same example using fix
+
+
+import numpy as np
+
+arr = np.fix([-3.1666, 3.6667])
+
+print(arr)
+
+
+
+import numpy as np
+
+arr = np.fix([4.55, 6.66])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.fix([5.666, 7.888])
+
+print(arr)
