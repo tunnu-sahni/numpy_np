a21fab14a3628a15646cd8c4bb7a7c9a79455967
tunnu-sahni <tsahni313@gmail.com>
2025-11-11 21:54:15 +0530

    added npdiffrence.py

---
+++ npdiffrence.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+arr = np.array([10,15, 25,5])
+
+newarr = np.diff(arr)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr = np.array([3,5,7,9,7])
+
+newarr = np.diff(arr)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr = np.array([54,6,76,87,98])
+
+newarr = np.diff(arr)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr = np.array([43,5,65])
+
+newarr = np.diff(arr)
+
+print(newarr)
+
+
+import numpy as np
+
+arr = np.array([54,65,76])
+
+newarr = np.diff(arr)
+
+print(newarr)
+
+
+
+# compute the discrete difference of the following array twice
+
+
+import numpy as np
+
+arr = np.array([10,15,25,5])
+
+newarr = np.diff(arr, n=2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr = np.array([20,30,49,50])
+
+newarr = np.diff(arr, n=2)
+
+print(newarr)
+
+
+
+import numpy as np
+
+arr = np.array([4,5,676,8])
+
+newarr = np.diff(arr, n=2)
+
+print(newarr)
