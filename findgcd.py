383386239847de80504a6c77b8a428282bc1b46e
tunnu-sahni <tsahni313@gmail.com>
2025-11-12 20:58:39 +0530

    added findgcd.py

---
+++ findgcd.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+num1 = 6
+num2 = 9
+
+x = np.gcd(num1, num2)
+
+print(x)
+
+
+import numpy as np
+
+num1 = 9
+num2 = 4
+
+x = np.gcd(num1, num2)
+
+print(x)
+
+
+import numpy as np
+
+num3 = 89
+num4 = 90
+
+x = np.gcd(num3, num4)
+
+print(x)
+
+
+
+import numpy as np
+
+num3 = 90
+num4 = 100
+
+x = np.gcd(num3, num4)
+
+print(x)
+
+
+# finding gcd in array
+
+import numpy as np
+
+arr = np.array([20, 8, 32, 36, 16])
+
+x = np.gcd.reduce(arr)
+
+print(x)
+
+
+
+import numpy as np
+
+arr = np.array([20, 30, 40,50])
+
+x = np.gcd.reduce(arr)
+
+print(x)
+
+
+
+import numpy as np
+
+arr = np.array([20,39,45])
+
+x = np.gcd.reduce(arr)
+
+print(x)
+
+
+
+import numpy as np
+
+arr = np.array([30,40,596,60])
+
+x = np.gcd.reduce(arr)
+
+print(x)
