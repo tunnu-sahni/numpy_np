291fb71d47c59d36c8bb30c360f4ee9e1b950ed0
tunnu-sahni <tsahni313@gmail.com>
2025-11-12 22:35:36 +0530

    added nphyperbolicfunc.py

---
+++ nphyperbolicfunc.py
@@ -0,0 +1,167 @@
+import numpy as np
+
+x = np.sinh(np.pi/2)
+
+print(x)
+
+
+import numpy as np
+
+x = np.sinh(np.pi/3)
+
+print(x)
+
+
+import numpy as np
+
+x = np.sinh(np.pi/7)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.sinh(np.pi/9)
+
+print(x)
+
+
+
+
+# find the cosh func
+
+
+import numpy as np
+
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+
+x = np.cosh(arr)
+
+print(x)
+
+
+
+import numpy as np
+
+arr = np.array([np.pi/5, np.pi/8])
+
+x = np.cosh(arr)
+
+print(x)
+
+
+import numpy as np
+
+arr = np.array([np.pi/77, np.pi/7, np.pi/5])
+
+x = np.cosh(arr)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.tanh(5)
+
+print(x)
+
+
+import numpy as np
+
+arr = np.array([np.pi/4, np.pi/5])
+
+x = np.tanh(arr)
+
+print(x)
+
+
+
+
+# finding angles
+
+
+import numpy as np
+
+x = np.arcsinh(1.0)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.arcsinh(2.5)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.arccosh(1.0)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.arccosh(2.4)
+
+print(x)
+
+
+import numpy as np
+
+x = np.arccosh(4)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.arctanh(4)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.arctanh(1.0)
+
+print(x)
+
+
+
+import numpy as np
+
+x = np.arctanh(5.6)
+
+print(x)
+
+
+
+# angles of each values in arrays
+
+
+import numpy as np
+
+arr = np.array([0.1, 0.2, 0.5])
+
+x = np.arctanh(arr)
+
+print(x)
+
+
+
+import numpy as np
+
+arr = np.arctanh([0.3, 0.4])
+
+x = np.arctanh(arr)
+
+print(x)
